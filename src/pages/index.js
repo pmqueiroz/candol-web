01146dac0e94b6ad9c936b854a11ac700064bb0b
pmqueiroz <pedroqueiroz593@gmail.com>
2020-11-26 08:13:37 -0300

    chore: clean up structure

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Candol</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      <h1>Hello Word</h1>
+    </div>
+  )
+}
